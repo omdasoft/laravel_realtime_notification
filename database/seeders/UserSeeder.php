9c40544f08dfcd9effdf862f0415c121266e8209
omdasoft <omdasoft@hotmail.com>
2024-12-10 22:32:59 +0400

    Add docker-compose.yml to gitignore & edit user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,21 +4,25 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-         //Create Admin User
-         User::factory()->create([
+        //Create Admin User
+        User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@test.com',
             'password' => 'admin',
-            'isAdmin' => 1
+            'isAdmin' => 1,
         ]);
 
-        //Create fake users
-        User::factory(10)->create();
+        //Create A User
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@test.com',
+            'password' => 'user',
+            'isAdmin' => 0,
+        ]);
     }
 }
